3e40f8b471f6c6e790822ca99f78ca2f731394b6
rassi0429 <taitokokoa+rassi@gmail.com>
2025-01-30 00:50:04 +0900

    chore

--- main.mjs
+++ main.mjs
@@ -5,9 +5,9 @@ const ENV = process.env.SAMPLE_ENV
 
 
 app.get("/", (req, res) => {
-    res.send("Hello, World! SAMPLE_ENV is" + ENV)
+    res.send("Hello, World! SAMPLE_ENV is " + ENV)
 })
 
-app.listen(3000, () => {
+app.listen(300, () => {
     console.log("Server is running on port 3000")
 })
